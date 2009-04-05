c343354a61bb47930bc80a354936481b49ffd410
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-04-05 12:04:33 +0200

    Definitions for Gtk2::UniqueMessageData

--- unique-perl.h
+++ unique-perl.h
@@ -4,6 +4,15 @@
 
 #include <unique/unique.h>
 
+
+/* Custom definitions for the bindings of Gtk2::UniqueMessageData */
+typedef UniqueMessageData UniqueMessageData_ornull;
+SV* newSVUniqueMessageData (UniqueMessageData *message);
+UniqueMessageData* SvUniqueMessageData (SV *data);
+# define SvUniqueMessageData_ornull(sv)	(gperl_sv_is_defined (sv) ? SvUniqueMessageData(sv) : NULL)
+# define newSVUniqueMessageData_ornull(val)	(((val) == NULL) ? &PL_sv_undef : gperl_new_object (G_OBJECT (val), FALSE))
+
+
 #include "unique-autogen.h"
 
 #endif /* _UNIQUE_PERL_H_ */
