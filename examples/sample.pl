2f7438a19bbe99909ded9c643211d424c09a336b
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-04-23 09:59:30 +0200

    Simplied the example

--- examples/sample.pl
+++ examples/sample.pl
@@ -7,46 +7,44 @@ use Glib qw(TRUE FALSE);
 use Gtk2 '-init';
 use Gtk2::Unique;
 
-my $COMMAND_PUSH = 1;
-my $COMMAND_POP = 2;
+my $COMMAND_WRITE = 1;
 
 
 exit main();
 
 
 sub main {
-	my ($command_id, $text) = parse_command(@ARGV);
+	die "Usage: message\n" unless @ARGV;
+	my ($text) = @ARGV;
 
 	# As soon as we create the UniqueApp instance we either have the name we
 	# requested ("org.mydomain.MyApplication", in the example) or we don't because
 	# there already is an application using the same name.
 	my $app = Gtk2::UniqueApp->new(
 		"org.mydomain.MyApplication", undef,
-		push => $COMMAND_PUSH,
-		pop  => $COMMAND_POP,
+		write => $COMMAND_WRITE,
 	);
 
 
 	# If there already is an instance running, this will return TRUE; there's no
 	# race condition because the check is already performed at construction time.
 	if ($app->is_running) {
-		my $response = send_command($app, $command_id, $text);
+		my $response = $app->send_message($COMMAND_WRITE, {text => $text});
 		print "Command sent\n";
 		return 0;
 	}
 
 
 	# Create the single application instance and wait for other requests
-	my $window = create_application($app, $command_id, $text);
-	Gtk2->main();
+	my $window = create_application($app, $text);
+	eval {
+		Gtk2->main();
+	};
+	if (my $error = $@) {
+		warn "Error: $error";
+	}
 	
 	return 0;
-}
-
-
-sub send_command {
-	my ($app, $command_id, $text) = @_;
-	my $response = $app->send_message($command_id, {text => $text});
 }
 
 
@@ -55,7 +53,7 @@ sub send_command {
 # other instance running.
 #
 sub create_application {
-	my ($app, $command_id, $text) = @_;
+	my ($app, $text) = @_;
 
 	# Standard window and windgets
 	my $window = Gtk2::Window->new();
@@ -65,9 +63,7 @@ sub create_application {
 	my $scroll = Gtk2::ScrolledWindow->new();
 	my $buffer = $textview->get_buffer;
 
-	if ($command_id == $COMMAND_PUSH) {
-		$buffer->insert($buffer->get_end_iter, "$text\n");
-	}
+	$buffer->insert($buffer->get_end_iter, "$text\n");
 
 	# Widget packing
 	$scroll->add($textview);
@@ -84,7 +80,7 @@ sub create_application {
 	$app->watch_window($window);
 	$app->signal_connect('message-received' => sub {
 		my ($app, $command, $message, $time) = @_;
-		$window->hide();		
+		$window->hide();
 		print "Got a command @_\n";
 		
 		# Must return a "Gtk2::UniqueResponse"
@@ -92,24 +88,3 @@ sub create_application {
 	});
 	return $window;
 }
-
-
-sub parse_command {
-	die "Usage: --push text | --pop\n" unless @_;
-	my ($operation, $text) = @_;
-	
-	my $command_id = 0;	
-	if ($operation eq '--push') {
-		die "Usage: --push text" unless defined $text;
-		$command_id = $COMMAND_PUSH;
-	}
-	elsif ($operation eq '--pop') {
-		$command_id = $COMMAND_POP;
-	}
-	else {
-		die "Invalid command: $operation\n";
-	}
-
-	return ($command_id, $text);
-}
-
